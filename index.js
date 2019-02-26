ff9f331548c45be331ba2d6c6bcf59ba401a710a
CodeNation <codenation@CodeNations-MacBook-Pro-2.local>
2019-02-26 15:50:31 +0000

    Fire sound

--- index.js
+++ index.js
@@ -199,3 +199,12 @@ window.onload = function() {
     loop(); 
   
 } 
+ 
+window.addEventListener('keydown', function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0; // rewind to the start of audio
+  audio.play();
+  key.classList.add('playing')
+});
